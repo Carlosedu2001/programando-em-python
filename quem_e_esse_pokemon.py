df7a511b25027176b3312645523b8e809b06b1db
Carlosedu2001 <kdu-lacerda@hotmail.com>
2022-10-24 22:31:28 -0300

    Updates

---
+++ quem_e_esse_pokemon.py
@@ -0,0 +1,20 @@
+import os, json, requests
+
+os.system("cls")
+
+def poke_habilidades(poke):
+
+    os.system("cls")
+    print(f"Habilidades do Pokémon {pokemon.title()}:\n")
+
+    for i in poke['abilities']:
+        print(i['ability']['name'])
+
+def main():
+    global pokemon
+    pokemon = str(input("Digite qual Pokémon deseja acessar as habilidades: "))
+    res = requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon.lower()}")
+    poke = res.json()
+    poke_habilidades(poke)
+
+main()
