bd6b85fe4a271335a20739b4597d4026bc2a6d08
Carlosedu2001 <kdu-lacerda@hotmail.com>
2022-11-15 14:35:57 -0300

    Updates

---
+++ hello_world.py
@@ -0,0 +1,22 @@
+from time import sleep
+import os
+
+os.system("cls")
+
+frase = "hello world"
+
+alphabet = list(" abcdefghijklmnopqrstuvwxyz")
+
+word = ""
+
+for i in range(0, len(frase)):
+    for l in range(0, len(alphabet)):
+        word = word + alphabet[l]
+        print(word)
+        sleep(0.05)
+        if frase[i] != word[i]:
+            word = word[:-1]
+        elif frase[i] == word[i]:
+            break
+    if frase == word:
+        break
